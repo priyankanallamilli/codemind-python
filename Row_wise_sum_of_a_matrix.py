d2cec6c66fd4afe71e5be5288e2bdcdbcb5b9b13
Priyankaa <96298451+9493191124@users.noreply.github.com>
2023-07-05 14:54:38 +0530

    r,c=map(int,input().split()) mat=[] for i in range(r):     lst=list(map(int,input().split()))     mat.append(lst) rsum=[] for i in range(r):     rsum.append(sum(mat[i])) print(*rsum)

---
+++ Row_wise_sum_of_a_matrix.py
@@ -0,0 +1,9 @@
+r,c=map(int,input().split())
+mat=[]
+for i in range(r):
+    lst=list(map(int,input().split()))
+    mat.append(lst)
+rsum=[]
+for i in range(r):
+    rsum.append(sum(mat[i]))
+print(*rsum)   
