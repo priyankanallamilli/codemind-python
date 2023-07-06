85f5326b52264c651da8a7bd0a0419e840579ee7
Priyankaa <96298451+9493191124@users.noreply.github.com>
2023-07-06 15:36:03 +0530

    def reverse(num):     rev=0     temp=num     while num:         d=num%10         num=num//10         rev=rev*10+d     if temp==rev:         return True     else:         return False num=int(input()) res=reverse(num) print(res)

--- Palindrome.py
+++ Palindrome.py
@@ -1,6 +1,14 @@
-n=input()
-a=n[::-1]
-if n==a:
-    print("True")
-else:
-    print("False")
+def reverse(num):
+    rev=0
+    temp=num
+    while num:
+        d=num%10
+        num=num//10
+        rev=rev*10+d
+    if temp==rev:
+        return True
+    else:
+        return False
+num=int(input())
+res=reverse(num)
+print(res)
