088ba7919569a91c76a0dbe4ad32393ec99f2337
Priyankaa <96298451+9493191124@users.noreply.github.com>
2023-07-10 10:49:15 +0530

    n=int(input()) l=list(map(int,input().split())) c=0 max=0 for i in l:     if len(str(i))>max:         max=len(str(i)) for i in l:     if len(str(i))==max:         c+=1 print(c)

---
+++ Max_digits_count.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+max=0
+for i in l:
+    if len(str(i))>max:
+        max=len(str(i))
+for i in l:
+    if len(str(i))==max:
+        c+=1
+print(c)
