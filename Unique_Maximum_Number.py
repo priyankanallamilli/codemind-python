95b7a2032ba6913e8c976c3ab52c21e993e90f98
Priyankaa <96298451+priyankanallamilli@users.noreply.github.com>
2024-07-30 09:14:42 +0530

    n=int(input()) l=list(map(int,input().split())) d={} for i in l:     if i in d:         d[i]+=1     else:         d[i]=1 arr=[k for k,v in d.items() if v==1] if not arr:     print("-1") else:     print(max(arr))

---
+++ Unique_Maximum_Number.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=list(map(int,input().split()))
+d={}
+for i in l:
+    if i in d:
+        d[i]+=1
+    else:
+        d[i]=1
+arr=[k for k,v in d.items() if v==1]
+if not arr:
+    print("-1")
+else:
+    print(max(arr))
